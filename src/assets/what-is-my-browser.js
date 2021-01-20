5d5190c9fce380566022421c5ce130e1a979af3a
Nimityx <c93c81b9-b581-4651-89fc-a416fd69558e@anonaddy.me>
2021-01-20 10:53:14 +0700

    Create what-is-my-browser.js

---
+++ src/assets/what-is-my-browser.js
@@ -0,0 +1,12 @@
+/*
+* what-is-my-browser.js
+* https://github.com/Nimityx/omnitools/blob/main/src/assets/what-is-my-browser.js
+* https://github.com/Nimityx/omnitools/blob/main/src/what-is-my-browser.html
+* https://nimityx.github.io/omnitools/what-is-my-browser.html
+* 
+* By Nimityx, https://github.com/Nimityx
+*
+* License : https://github.com/Nimityx/omnitools/blob/main/LICENSE (AGPL-3.0)
+* source  : https://github.com/Nimityx/omnitools
+*/
+document.getElementById("browser").innerHTML = navigator.userAgent;
